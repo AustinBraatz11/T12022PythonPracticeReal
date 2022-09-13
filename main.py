4bf829b8a53d5f8cb84801eb93f3303ad8f3506f
AustinBraatz11 <826010@mystma.org>
2022-09-13 09:41:01 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,13 @@
+def sayMyName():
+    print("Austin Braatz")
+def topFiveAnimals():
+    print("5. dog")
+    print("4. dog")
+    print("3. dog")
+    print("2. dog")
+    print("1. fox")
+def doYouKnowWhoElseLikesCoding():
+    print("My Mom!")
+sayMyName()
+topFiveAnimals()
+doYouKnowWhoElseLikesCoding()
